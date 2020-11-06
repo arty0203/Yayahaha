bcd60834129d52f7e9b2af1c40c4c384f00cc4f3
ARTY <624708020009@dpu.ac.th>
2020-11-06 23:25:19 +0700

    add controller again

--- controllers/carController.js
+++ controllers/carController.js
@@ -34,5 +34,48 @@ controller.add = async (req, res) => {
         });
     }
 }
+controller.getByID = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const car = await CarModel.findOne({ id });
+        res.json({ id: car })
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error
+        });
+    }
+}
+
+controller.UpdateByID = async (req, res) => {
+    try {
+        const id = req.params.id;
+        var name = req.body.name
+        var model = req.body.model
+        var weight = req.body.weight
+        var color = req.body.color
+        const cars = await CarModel.findOneAndUpdate({ id }, { name, model, weight, color });
+        res.json({ name: cars })
+    } catch (error) {
+        res.status(400).json({
+            status: "ว๊ายผิด",
+            message: error
+        })
+    }
+}
+
+controller.deleteByID = async (req, res) => {
+    try {
+        const id = req.params.id
+        const cars = await CarModel.findOneAndDelete({ id });
+        res.json({ data: cars })
+
+    } catch (error) {
+        res.status(400).json({
+            status: "ว๊ายผิด",
+            message: error
+        })
+    }
+}
 
 module.exports = controller;
--- routes/carRoute.js
+++ routes/carRoute.js
@@ -9,48 +9,11 @@ router.get('/', controller.getAll);
 
 router.post('/add', controller.add);
 
-router.get('/:id',async(req,res)=>{
-    try {
-        const id = req.params.id;
-        const car = await CarModel.findOne({ id });
-        res.json({id: car})
-    } catch (error) {
-        res.status(404).json({
-            status: "fail",
-            message: error
-        });
-    }
-})
+router.get('/:id',controller.getByID);
 
-router.put('/:id',async(req,res)=>{
-    try {
-        const id = req.params.id;
-        var name = req.body.name
-        var model = req.body.model
-        var weight = req.body.weight
-        var color = req.body.color
-        const cars = await CarModel.findOneAndUpdate({id}, {name,model,weight, color});
-        res.json({ name: cars })
-    } catch (error) {
-        res.status(400).json({
-            status:"ว๊ายผิด",
-            message: error
-        })
-    }
-})
 
-router.delete('/:id',async(req,res)=>{
-    try {
-        const id = req.params.id
-        const cars = await CarModel.findOneAndDelete({ id });
-        res.json({data:cars})
-        
-    } catch (error) {
-        res.status(400).json({
-            status:"ว๊ายผิด",
-            message: error
-        })
-    }
-})
+router.put('/:id',controller.UpdateByID)
+
+router.delete('/:id',controller.deleteByID)
 
 module.exports = router;
